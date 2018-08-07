24b18f42908ad9d6b6f0c7a8d063a7af13e1d8a1
amlmsh <martin.huelse@gmx.net>
2018-08-07 12:02:09 +0200

    new structure and added ardunio programm for real servo motors

---
+++ PanTilt/PanTilt/PanTilt.ino
@@ -0,0 +1,72 @@
+#include <Servo.h>
+
+const unsigned long BAUD_RATE = 9600;
+unsigned int valueA = 1;
+unsigned int valueB = 1;
+unsigned int valueTmp;
+char cTmp;
+
+// servo
+Servo servo_1;
+Servo servo_2;
+const unsigned int SERVO_PAN  = 1;
+const unsigned int SERVO_TILT = 2;
+const unsigned int MIN_SERVO_ANGLE = 2;
+const unsigned int MAX_SERVO_ANGLE = 180;
+const unsigned char MOTOR_PIN_1 = 9;
+const unsigned char MOTOR_PIN_2 = 10;
+const unsigned int  MOTOR_PAUSE = 10;
+
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  servo_1.attach(MOTOR_PIN_1);
+  servo_1.write(45);
+  servo_2.attach(MOTOR_PIN_2);
+  servo_2.write(45);
+  delay(MOTOR_PAUSE);
+}
+
+
+void writeServo(unsigned int position, int servo){
+  unsigned int targetPosition;
+
+  if (position < MIN_SERVO_ANGLE) {
+    targetPosition = MIN_SERVO_ANGLE;
+  } else if (position > MAX_SERVO_ANGLE) {
+    targetPosition = MAX_SERVO_ANGLE;
+  } else{
+    targetPosition = position;
+  }
+
+  if(servo == SERVO_PAN){
+    servo_1.write(targetPosition);
+  }else{
+    servo_2.write(targetPosition);
+  }
+}
+
+
+void serialEvent() {
+  if (Serial.available()) {
+    valueA = Serial.parseInt();
+    valueB = Serial.parseInt();
+    delay(10);
+    Serial.print(valueA);
+    Serial.print('X');
+    Serial.print(valueB);
+    Serial.print('X');
+
+    writeServo(valueA, SERVO_PAN);
+    writeServo(valueB, SERVO_TILT);
+  }
+
+}
+
+void loop() {
+
+}
